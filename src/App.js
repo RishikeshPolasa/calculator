1b3dc673e61eb0a42f3f734ad79fd52bc81b59ad
rishiZS <rishikesh.polasa@zopsmart.com>
2023-10-02 21:47:25 +0530

    added functionality of calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [sign, setSign] = useState("+");
+  const [input1, setInput1] = useState();
+  const [input2, setInput2] = useState();
+  const [result, setResult] = useState(0);
+  const [count, setCount] = useState(0);
+  const submitResult = () => {
+    let tempResult;
+    switch (sign) {
+      case "+":
+        tempResult = parseInt(input1) + parseInt(input2);
+        break;
+      case "-":
+        tempResult = parseInt(input1) - parseInt(input2);
+        break;
+      case "*":
+        tempResult = parseInt(input1) * parseInt(input2);
+        break;
+      default:
+        tempResult = parseInt(input1) % parseInt(input2);
+        break;
+    }
+    setResult(tempResult);
+    let tempCount = count;
+    tempCount++;
+    setCount(tempCount);
+  };
+
+  const changeSign = (e) => {
+    const curSign = e.target.textContent;
+    switch (curSign) {
+      case "+":
+        setSign("+");
+        break;
+      case "-":
+        setSign("-");
+        break;
+      case "*":
+        setSign("*");
+        break;
+      default:
+        setSign("%");
+        break;
+    }
+  };
+
+  const handleReset = () => {
+    setResult(0);
+    setInput1("");
+    setInput2("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Total Count:{count}</div>
+      <div className="InputWrapper">
+        <input
+          name="input1"
+          value={input1}
+          onChange={(e) => setInput1(e.target.value)}
+        />
+        <div className="SignValue">{sign}</div>
+        <input
+          name="input2"
+          value={input2}
+          onChange={(e) => setInput2(e.target.value)}
+        />
+      </div>
+      <div className="SignButtons">
+        <div className="Plus" onClick={(e) => changeSign(e)}>
+          +
+        </div>
+        <div className="Plus" onClick={(e) => changeSign(e)}>
+          -
+        </div>
+        <div className="Plus" onClick={(e) => changeSign(e)}>
+          *
+        </div>
+        <div className="Plus" onClick={(e) => changeSign(e)}>
+          %
+        </div>
+      </div>
+      <div className="ResetAndResultWrapper">
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={submitResult}>Result{result}</button>
+      </div>
     </div>
   );
 }
